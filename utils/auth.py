3a200b449f4a66b06330a0ba46a310234f43581e
Tripathisourabh <sourabh.tripathi@opslyft.com>
2022-09-06 15:38:44 +0530

    switch assume role account to opslyft master account

--- utils/auth.py
+++ utils/auth.py
@@ -4,7 +4,7 @@ from credentials import accountid
 def assume_role():
     sts_client = boto3.client('sts')
     assumed_role_object=sts_client.assume_role(
-        RoleArn=f"arn:aws:iam::544089724024:role/prometheus-role-{accountid}",
+        RoleArn=f"arn:aws:iam::612488371952:role/prometheus-role-{accountid}",
         RoleSessionName="AssumeRoleSession1"
     )
     credentials=assumed_role_object['Credentials']
