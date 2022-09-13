7f9ed835ef306b3804111c2bd6d3bd97a8d68620
Tripathisourabh <sourabh.tripathi@opslyft.com>
2022-09-14 02:03:54 +0530

    change role name

--- utils/auth.py
+++ utils/auth.py
@@ -1,10 +1,10 @@
 import  boto3
-from credentials import accountid
+from credentials import accountid, stackid
 
 def assume_role():
     sts_client = boto3.client('sts')
     assumed_role_object=sts_client.assume_role(
-        RoleArn=f"arn:aws:iam::612488371952:role/prometheus-role-{accountid}",
+        RoleArn=f"arn:aws:iam::612488371952:role/prometheus-roles-{accountid}/{stackid}",
         RoleSessionName="AssumeRoleSession1"
     )
     credentials=assumed_role_object['Credentials']
